27e9ea9dba3b9b935365b02ab0026a0328bbdb63
jezyk <zrarbi@o2.pl>
2016-03-17 08:32:53 +0100

    Added files via upload

--- program4.py
+++ program4.py
@@ -13,6 +13,8 @@ from functions import running_avr
 from matplotlib.colors import LogNorm
 import matplotlib.font_manager as font_manager
 
+rms_L_SP = 3.  # === N sigma level in every SP
+
 
 def nintd(x):
     nintd = int(math.floor(x))
@@ -84,18 +86,12 @@ for el in xrange(len(I)):
     s = s + delbin
 print c,el
 
-#print rem
-
+print 'BASIC CALCULATIONS ---> DONE'
 
 Irem = np.delete(I,rem)
 Qrem = np.delete(Q,rem)
 Urem = np.delete(U,rem)
 Vrem = np.delete(V,rem)
-#print len(I), len(Irem), len(I)-len(Irem)
-#print type(Irem), type(Qrem),type(Urem),type(Vrem)
-
-#for i in xrange(5):
-#    print Irem[i], Qrem[i], Urem[i], Vrem[i]
 
 
 Irem = Irem[:int(nper)*int(nb)]
@@ -109,14 +105,12 @@ i2d = np.reshape(Irem, (len(Irem)/int(nb),int(nb)))
 q2d = np.reshape(Qrem, (len(Qrem)/int(nb),int(nb)))
 u2d = np.reshape(Urem, (len(Urem)/int(nb),int(nb)))
 v2d = np.reshape(Vrem, (len(Vrem)/int(nb),int(nb)))
+
+
+print 'REBINNING AND RESHAPING ---> DONE'
+
 #i2dlist = i2d.tolist()
 print 'new shape dimension',i2d.shape,q2d.shape, u2d.shape, v2d.shape   # type(i2d)#, type(i2dlist)
-
-#print i2d,q2d,u2d,v2d
-
-
-#su = np.apply_along_axis(np.mean, 0, i2d)
-#sut= np.apply_along_axis(np.mean, 0, i2dtemptolist)
 
 
 step_sdv = 100
@@ -133,6 +127,8 @@ sdv1 = np.apply_along_axis(running_avr, 1, v2d, step_sdv, 1) #stdev of Stokes V 
 #print sdi0, type(sdi0), sdi0.shape
 #print sdi1, type(sdi1), sdi1.shape
 
+print 'AVERAGE AND STDDEV IN STOKES ---> DONE'
+
 
 min_sdv = np.apply_along_axis(min, 1, sdi1)
 min_pos = np.apply_along_axis(np.argmin, 1, sdi1)
@@ -147,6 +143,7 @@ min_sdv_v = np.apply_along_axis(min, 1, sdv1)
 min_pos_v = np.apply_along_axis(np.argmin, 1, sdv1)
 
 
+print 'AVERAGE STOKES ---> DONE'
 
 min_val_i = []
 for i, arr in zip(min_pos, sdi0):
@@ -165,18 +162,18 @@ for i, arr in zip(min_pos_v, sdv0):
      min_val_v.append(arr[i])
 
 
-
-#print min_val_i
-
-
 i2d_b = (i2d.T - min_val_i).T  # i2d minus baseline in each SP separately
 q2d_b = (q2d.T - min_val_q).T  # q2d minus baseline
 u2d_b = (u2d.T - min_val_u).T  # u2d minus baseline
 v2d_b = (v2d.T - min_val_v).T  # v2d minus baseline
 #print i2d_bT.shape
 
+
+print 'REBINNED AND RESHAPED - BASELINE ---> DONE'
+
+
 #print i2d_b.shape,i2d_b
-sdi2 = np.apply_along_axis(np.mean, 0, i2d_b) # mean Stokes
+sdi2 = np.apply_along_axis(np.mean, 0, i2d_b) # mean Stokes === average profile
 sdq2 = np.apply_along_axis(np.mean, 0, q2d_b)
 sdu2 = np.apply_along_axis(np.mean, 0, u2d_b)
 sdv2 = np.apply_along_axis(np.mean, 0, v2d_b)
@@ -189,69 +186,34 @@ SIEW = siew[min_pos_siew]
 print min_pos_siew, SIEW
 print sdi2.shape
 
+
+
 #print L, L_b
-
-
-
-
-
-# total polarisation intensity
-#tot_pol_int = np.sqrt(np.array(q2d**2 + u2d**2 + v2d**2))
-
-#print tot_pol_int.shape
-
-# total polarisation fraction
-#tot_pol_frac = tot_pol_int / i2d
-
-#print tot_pol_int, tot_pol_frac
-
 
 
 # linear polarisation
 L = np.sqrt(q2d**2 + u2d**2)
-#L_b = np.sqrt(q2d_b**2 + u2d_b**2)
+L_b2 = np.sqrt(q2d_b**2 + u2d_b**2)
 L_b = np.sqrt(sdq2**2 + sdu2**2)
 
+print 'L_b.shape',L_b.shape, L_b2.shape
+sdL_b20 = np.apply_along_axis(running_avr, 1, L_b2, step_sdv, 0)
+sdL_b21 = np.apply_along_axis(running_avr, 1, L_b2, step_sdv, 1)
+
+min_sdv_L_b2 = np.apply_along_axis(min, 1, sdL_b21)
+min_pos_L_b2 = np.apply_along_axis(np.argmin, 1, sdL_b21)
+
+min_val_L_b2 = []
+for i, arr in zip(min_pos_L_b2, sdL_b20):
+     min_val_L_b2.append(arr[i])
 
 
+sdL2d = min_sdv_L_b2# * rms_L_SP
+print sdL2d
 
+#print min_pos_L_b2, min_val_L_b2, min_val_L_b2[0], sdL_b20[0,min_pos_L_b2[0]]
+print sdL2d.shape
 
-#print tot_pol_frac.shape
-
-meanI = np.apply_along_axis(np.mean, 0, i2d)
-#meanI_b = np.apply_along_axis(np.mean, 0, sdi2)
-
-meanQ = np.apply_along_axis(np.mean, 0, q2d)
-meanQ_b = np.apply_along_axis(np.mean, 0, sdq2)
-
-meanU = np.apply_along_axis(np.mean, 0, u2d)
-meanU_b = np.apply_along_axis(np.mean, 0, sdu2)
-
-meanV = np.apply_along_axis(np.mean, 0, v2d)
-#meanV_b = np.apply_along_axis(np.mean, 0, sdv2)
-
-#meanL = np.apply_along_axis(np.mean, 0, L)
-meanL_b = np.apply_along_axis(np.mean, 0, L_b)
-
-LQU = np.sqrt(meanQ_b**2 + meanU_b**2)
-
-#sdL0 = running_avr(meanL, step_sdv, 0)
-#sdL1 = running_avr(meanL, step_sdv, 1)
-
-#min_sdv_L = np.min(sdL1)
-#min_pos_L = np.argmin(sdL1)
-
-#min_val_L = []
-#for i, arr in zip(min_pos_L, meanL):
-#     min_val_L.append(arr[i])
-L#2d_b = meanL - sdL0[min_pos_L]
-
-#meanL2d_b = np.apply_along_axis(np.mean, 0, L2d_b)
-#print min_sdv_L, min_pos_L, sdL0[min_pos_L], L2d_b.shape
-
-#print L_b.shape, L_b
-
-#print len(L_b)
 
 # ==========================================
 # polarisation L Everett, Weisberg & Mitra, Li
@@ -265,7 +227,11 @@ LEW[L_b/SIEW >= 1.57] = np.sqrt((L_b[L_b/SIEW >= 1.57])**2-SIEW**2)
 #print LEW
 
 LML = L_b - SIEW
+
+
 # ==========================================
+
+print 'CIRCULAR POLARISATION ---> DONE'
 
 #mean_tot_pol_frac = np.apply_along_axis(np.mean, 0, tot_pol_frac)
 #maxL= np.apply_along_axis(max, 1, L)
@@ -276,10 +242,12 @@ LML = L_b - SIEW
 #    uppa = 0.0
 #    qppa = 0.0
 
-#    uppa = np.apply_along_axis(sum, 0, u2d)
-#    qppa = np.apply_along_axis(sum, 0, q2d) 
+#    uppa = np.apply_along_axis(sum, 0, u2d_b)
+#    qppa = np.apply_along_axis(sum, 0, q2d_b) 
 
-#    ppa = np.array (0.5 * np.arctan2(uppa,qppa))#**(-1.)
+ppa = np.array (0.5 * np.arctan2(u2d_b,q2d_b))#**(-1.)
+#ppa1 = np.array (0.5 * np.arctan(u2d_b/q2d_b))#**(-1.)
+print ppa.shape
 #    print ppa
     
 #print np.degrees(ppa), ppa.shape
@@ -291,12 +259,14 @@ LML = L_b - SIEW
 
 
 
+print 'PPA ---> DONE'
+
 
 
 
 
 plt.subplots_adjust(hspace=0.1)
-#plt.subplot(211)
+#plt.subplot(311)
 ##plt.xlim([700,900])
 #plt.xlim([0,nb])
 #plt.ylim([0,nper])
@@ -305,7 +275,7 @@ plt.subplots_adjust(hspace=0.1)
 #y=[0.,nper]
 #plt.plot(x,y)
 
-plt.subplot(212)
+plt.subplot(312)
 #plt.xlim([700,900])
 plt.xlim([0,nb])
 plt.plot(sdi2, label='meanI_b')
@@ -317,11 +287,12 @@ plt.plot(sdi2, label='meanI_b')
 #plt.plot(sdu2, label='meanU_b')
 #plt.plot(meanV, label='meanV')
 plt.plot(sdv2, label='meanV_b')
-#plt.plot(meanL, label='meanL')
+#plt.plot(meanL_b2, label='meanL_b2')
 #plt.plot(meanL_b, label='meanL_b')
-#plt.plot(LEW, label='LEW', lw=0.5)
+#plt.plot(LEW, label=r'${\mathrm{L_{EW}}}$', lw=0.5)
+plt.plot(LEW, label=r'${L_{EW}}$', lw=0.5)
 #plt.plot(LML, label='LML', lw=0.5)
-plt.plot(LQU, label='LQU', lw=0.5)
+#plt.plot(LQU, label='LQU', lw=0.5)
 #plt.plot(L2d_b, label='L-b', lw=0.5)
 
 #plt.plot(meanV**2, label='meanV2')
@@ -330,18 +301,31 @@ plt.plot(LQU, label='LQU', lw=0.5)
 plt.legend(loc='upper left',prop=font_manager.FontProperties(size=10))
 
 #plt.plot(L.T)
-plt.savefig('stokes.ps', bbox_inches='tight')
+#plt.savefig('stokes.ps', bbox_inches='tight')
 #plt.show()
 
 
-plt.subplots_adjust(hspace=0.1)
-plt.subplot(211)
-plt.plot(np.degrees(ppa),  label='ppa', color='red')
+plt.subplots_adjust(hspace=0.3)
+plt.subplot(313)
+plt.xlim([0,nb])
+#plt.plot(np.degrees(ppa),  label='PPA - arctan', color='r', marker='.', linestyle='none')
+#plt.plot(np.degrees(ppa1), label='PPA - arctan2', color='b', marker='.', linestyle='none')
+plt.xlabel('BIN')
+plt.ylabel(r'PPA [$^{\circ}$]')
+plt.title('Polarisation Position Angle')
+#plt.legend(loc='upper left',prop=font_manager.FontProperties(size=8))
+
+#for i in xrange (u2d_b.shape[0]):
+for i in xrange(1):
+    plt.plot (np.degrees(ppa[i,]), marker='.', markersize=0.1, linestyle='none', color='b')
+    print i
+
 
 #plt.savefig('ppa.ps', bbox_inches='tight')
 #plt.show()
 
 
+plt.savefig('stokes.ps', bbox_inches='tight')
 
 
 
